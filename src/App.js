d4d7bc70f31ce1fc1b593fa45aedefa602969740
alicearchhive <tldhselwkdls@naver.com>
2020-08-11 20:59:22 +0900

    ert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
-<p> hello ! </p>
+    <div>
+    <p> 안녕하 세요 ! </p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33}/>
+    </div>
   );
 }
 
