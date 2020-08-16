33c71987ff14e4d5f37a2c7b5be4d8b424eac1b0
alicearchhive <tldhselwkdls@naver.com>
2020-08-16 17:06:00 +0900

    0816

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Icon, Image, Grid, Divider, Header } from "semantic-ui-react";
+
+import o from "./o.jpg";
 
 function App() {
   return (
-    <div>
-    <p> 안녕하 세요 ! </p>
-    <Button Primary> hi </Button>
-    <Progress percent = {33} indicating/>
-    </div>
+    <Grid centered>
+      <br />
+      <div>
+        <Grid.Row>
+          <p> 안녕하세요 ! </p>
+        </Grid.Row>
+        <Button Primary> hi </Button>
+        <Grid.Row>
+          <br />
+          <Image src={o} centered />
+        </Grid.Row>
+
+        <Divider horizontal>
+          <Header as="h4">contact me</Header>
+        </Divider>
+
+        <div>
+          <Button
+            circular
+            color="facebook"
+            icon="facebook"
+            onClick={() => window.open("https://blog.naver.com/tldhselwkdls")}
+          />
+          <Button circular color="twitter" icon="twitter" />
+          <Button circular color="linkedin" icon="linkedin" />
+          <Button circular color="google plus" icon="google plus" />
+        </div>
+      </div>
+    </Grid>
   );
 }
 
